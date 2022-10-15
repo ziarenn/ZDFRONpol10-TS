947748fceb2a1278b84602a95522b8b757c472df
Kacper Żarna <kacper@192.168.1.18>
2022-10-15 12:08:52 +0200

    Kod 15.10.2022 12:00

---
+++ dist/App.js
@@ -0,0 +1,8 @@
+// 1. PODSTAWOWE TYPY DANYCH
+// let
+let age;
+age = 40;
+console.log(age);
+// age = "lalala"; BLAD
+// const
+const firstName = "John";
---
+++ src/App.ts
@@ -0,0 +1,12 @@
+// 1. PODSTAWOWE TYPY DANYCH
+
+// let
+
+let age: number;
+
+age = 40;
+console.log(age);
+// age = "lalala"; BLAD
+
+// const
+const firstName = "John";
